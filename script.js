873118df81c6bf0b90a30f67d0d257f001872b51
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-12 20:08:30 +0545

    background added

--- script.js
+++ script.js
@@ -5,11 +5,29 @@ const nav = document.querySelector(".top");
 function handelEnter() {
   this.classList.add("trigger-enter");
   setTimeout(() => this.classList.add("trigger-enter-active"), 150);
-  //   console.log("entered");
+  background.classList.add("open");
+
+  const dropdown = this.querySelector(".dropdown");
+  const data = dropdown.getBoundingClientRect();
+  const coords = {
+    width: data.width,
+    height: data.height,
+    top: data.top,
+    left: data.left,
+  };
+  background.style.setProperty("height", `${coords.height}px`);
+  background.style.setProperty("width", `${coords.width}px`);
+  background.style.setProperty(
+    "transform",
+    `translate(${coords.left}px,${coords.top}px)`
+  );
+  console.log(data);
 }
 
 function handelLeave() {
   this.classList.remove("trigger-enter", "trigger-enter-active");
+  background.classList.remove("open");
+
   //   console.log("left");
 }
 
