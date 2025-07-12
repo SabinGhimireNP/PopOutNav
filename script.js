70722cffdd6b79b726d5be0b48af2a238ad66862
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-12 20:30:12 +0545

    touchUps

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ const triggers = document.querySelectorAll(".cool > li");
 const background = document.querySelector(".dropdownBackground");
 const nav = document.querySelector(".top");
 
-function handelEnter() {
+function handleEnter() {
   this.classList.add("trigger-enter");
   setTimeout(() => this.classList.add("trigger-enter-active"), 150);
   background.classList.add("open");
 
   const dropdown = this.querySelector(".dropdown");
   const data = dropdown.getBoundingClientRect();
+  const navdata = nav.getBoundingClientRect();
   const coords = {
     width: data.width,
     height: data.height,
-    top: data.top,
-    left: data.left,
+    top: data.top - navdata.top,
+    left: data.left - navdata.left,
   };
   background.style.setProperty("height", `${coords.height}px`);
   background.style.setProperty("width", `${coords.width}px`);
@@ -24,7 +25,7 @@ function handelEnter() {
   console.log(data);
 }
 
-function handelLeave() {
+function handleLeave() {
   this.classList.remove("trigger-enter", "trigger-enter-active");
   background.classList.remove("open");
 
@@ -32,8 +33,8 @@ function handelLeave() {
 }
 
 triggers.forEach((trigger) =>
-  trigger.addEventListener("mouseenter", handelEnter)
+  trigger.addEventListener("mouseenter", handleEnter)
 );
 triggers.forEach((trigger) =>
-  trigger.addEventListener("mouseleave", handelLeave)
+  trigger.addEventListener("mouseleave", handleLeave)
 );
